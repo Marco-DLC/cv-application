dd92a4385b7522e2c37f822ef3b7a039dd87e6e3
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-07-01 15:53:42 -0400

    Add remaining Form elements and create states
    The remaining elements for the form including Fields and Inputs have
    been added. States have been created for each Field, and their
    appropriate props have been assigned to their respective Inputs. I also
    deleted a few files that were leftover from the React Vite template.

--- src/Form.jsx
+++ src/Form.jsx
@@ -7,9 +7,24 @@ export default function Form() {
     userPhoneNum: "",
   });
 
+  const [educationalInfo, setEducationalInfo] = useState({
+    schoolName: "",
+    studyTitle: "",
+    studyDate: "",
+  });
+
+  const [careerInfo, setCareerInfo] = useState({
+    companyName: "",
+    jobTitle: "",
+    jobDesc: "",
+    startDate: "",
+    endDate: "",
+  });
+
   return (
     <div className="cv-form">
       <form>
+        <h1>CV Application</h1>
         <Field title="General Information">
           <Input
             label="Full Name"
@@ -17,7 +32,92 @@ export default function Form() {
             onChange={(e) =>
               setGenInfo({ ...genInfo, userName: e.target.value })
             }
-          ></Input>
+          />
+          <Input
+            label="Email Address"
+            value={genInfo.userEmail}
+            onChange={(e) =>
+              setGenInfo({ ...genInfo, userEmail: e.target.value })
+            }
+          />
+          <Input
+            label="Phone Number"
+            value={genInfo.userPhoneNum}
+            onChange={(e) =>
+              setGenInfo({ ...genInfo, userPhoneNum: e.target.value })
+            }
+          />
+        </Field>
+
+        <Field title="Education Experience">
+          <Input
+            label="School Name"
+            value={educationalInfo.schoolName}
+            onChange={(e) =>
+              setEducationalInfo({
+                ...educationalInfo,
+                schoolName: e.target.value,
+              })
+            }
+          />
+          <Input
+            label="Title of Study"
+            value={educationalInfo.studyTitle}
+            onChange={(e) =>
+              setEducationalInfo({
+                ...educationalInfo,
+                studyTitle: e.target.value,
+              })
+            }
+          />
+          <Input
+            label="Date of Study"
+            value={educationalInfo.studyDate}
+            onChange={(e) =>
+              setEducationalInfo({
+                ...educationalInfo,
+                studyDate: e.target.value,
+              })
+            }
+          />
+        </Field>
+
+        <Field title="Career Experience">
+          <Input
+            label="Company Name"
+            value={careerInfo.companyName}
+            onChange={(e) =>
+              setCareerInfo({ ...careerInfo, companyName: e.target.value })
+            }
+          />
+          <Input
+            label="Job Title"
+            value={careerInfo.jobTitle}
+            onChange={(e) =>
+              setCareerInfo({ ...careerInfo, jobTitle: e.target.value })
+            }
+          />
+          <Input
+            label="Job Responsibilities"
+            value={careerInfo.jobDesc}
+            onChange={(e) =>
+              setCareerInfo({ ...careerInfo, jobDesc: e.target.value })
+            }
+          />
+          <Input
+            label="Start Date"
+            value={careerInfo.startDate}
+            onChange={(e) =>
+              setCareerInfo({ ...careerInfo, startDate: e.target.value })
+            }
+          />
+          <Input
+            label="End Date"
+            value={careerInfo.endDate}
+            onChange={(e) =>
+              setCareerInfo({ ...careerInfo, endDate: e.target.value })
+            }
+          />
         </Field>
       </form>
     </div>
